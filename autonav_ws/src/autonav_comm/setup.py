59ab13d50d9da820b982dfe1964b6741fc5cdf25
Dylan Zemlin <dylan.zemlin@gmail.com>
2022-09-20 19:08:36 -0500

    Generated autonav_comm package

---
+++ autonav_ws/src/autonav_comm/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'autonav_comm'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*.xml')))
+    ],
+    install_requires=['setuptools'],
+    maintainer='',
+    maintainer_email='dylan.zemlin@gmail.com',
+    description='An empty AutoNav ROS package',
+    license='MIT License',
+    entry_points={}
+)
