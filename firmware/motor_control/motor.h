82a01d702388e53671b80bcf8c6a6a14d1b8ae53
j-exinia <exinia@ou.edu>
2023-02-16 21:14:29 -0600

    Changed pins on pico to account for PWM pin hardware change

--- firmware/motor_control/motor.h
+++ firmware/motor_control/motor.h
@@ -6,26 +6,25 @@
 #define ServoMinWidth 1000
 #define ServoMaxWidth 2000
 
-#define MOTOR_UPDATE_RATE 100 // Frequency that motor PID is updated (Hz)
-#define MAX_SPEED 2.2f // (m/s)
-#define PULSES_PER_REV (600.0f * 7 * 2) // (revs)
+#define MOTOR_UPDATE_RATE 100            // Frequency that motor PID is updated (Hz)
+#define MAX_SPEED 2.2f                   // (m/s)
+#define PULSES_PER_REV (600.0f * 7 * 2)  // (revs)
 //#define LINEAR_PER_REV 0.2054f // Wheel radius (m) old
-#define LINEAR_PER_REV 0.2032f // Wheel radius (m)
-#define MILLIS_TO_FULL 250 // Milliseconds to go from 0 output speed to 1
-#define LPIIR_DECAY 0.1f // Decay rate of low pass filter on velocity
+#define LINEAR_PER_REV 0.2032f  // Wheel radius (m)
+#define MILLIS_TO_FULL 250      // Milliseconds to go from 0 output speed to 1
+#define LPIIR_DECAY 0.1f        // Decay rate of low pass filter on velocity
 
-#define PI 3.14159265f
 #define INCREMENT_AMT (1000.0f / (MILLIS_TO_FULL * MOTOR_UPDATE_RATE))
 
-#define PREV_MASK 0x1 // Mask for the previous state in determining direction of rotation.
-#define CURR_MASK 0x2 // Mask for the current state in determining direction of rotation.
-#define INVALID   0x3 // XORing two states where both bits have changed.
+#define PREV_MASK 0x1  // Mask for the previous state in determining direction of rotation.
+#define CURR_MASK 0x2  // Mask for the current state in determining direction of rotation.
+#define INVALID 0x3    // XORing two states where both bits have changed.
 
 #define DEADZONE 5
 
 class motor {
   int PWM_Pin;
-  
+
   bool disabled = LOW;
   int defaultOnZero = HIGH;
 
@@ -33,7 +32,7 @@ class motor {
   float currentOutput = 0.0f;
   float currentSpeed = 0.0f;
   float speedEstimate = 0.0f;
-  
+
   // PID
   float integrator = 0.0f;
   float error = 0.0f;
@@ -46,184 +45,166 @@ class motor {
   volatile int pulses = 0;
 
   volatile int delta = 0;
-  
+
   // equations
   float kp = 0.05f;
   float ki = 0.0f;
-  float kd = 0.0f;  
+  float kd = 0.0f;
   bool reverse = false;
-  
+
   Servo motorServo;
-  public:
-    motor(int PWM_Motor, bool reverse = false)
+public:
+  motor(int PWM_Motor, bool reverse = false) {
+    this->PWM_Pin = PWM_Motor;
+    this->reverse = reverse;
+  }
+
+  void  setup() {
+    this->motorServo.attach(this->PWM_Pin, ServoMinWidth, ServoMaxWidth);
+  }
+
+  void output(float speed) {
+    if (this->reverse) {
+      //speed = -speed;
+    }
+    this->targetSpeed = speed;
+  }
+  //stolen from igvc 21 / QEI library
+  void pulse(int left, int right) {
+    //Serial.printf("left %d, right %d \n", left, right);
+    currState_ = (left << 1) | (right);
+
+    //11->00->11->00
+    //11->00->11->00 is counter clockwise rotation or "forward".
+    if ((prevState_ == 0x3 && currState_ == 0x0) || (prevState_ == 0x0 && currState_ == 0x3)) {
+      this->pulses++;
+      this->delta++;
+    }
+    //10->01->10->01 is clockwise rotation or "backward".
+    else if ((prevState_ == 0x2 && currState_ == 0x1) || (prevState_ == 0x1 && currState_ == 0x2)) {
+      this->pulses--;
+      this->delta--;
+    }
+
+    prevState_ = currState_;
+    if (currentOutput < 0.01f && currentOutput > -0.01f) {  // break
+    }
+  }
+  void brake() {
+    this->motorServo.write(90);
+  }
+  int out2servo(float out) {
+
+    if (this->reverse)
+      out = -out;
+    out = out / MAX_SPEED;
+    int servoOut;
+    //Serial.printf("servo in %f \n",out);
+    if (out < 0.01f && out > -0.01f) {  // break
+      servoOut = 90;
+    } else if (currentOutput > 0.0f) {  // forward
+      out = out * 85;
+      servoOut = (int)(90 + DEADZONE + out);
+    } else  //backward
     {
-      this->PWM_Pin = PWM_Motor;
-      this->motorServo.attach(this->PWM_Pin, ServoMinWidth, ServoMaxWidth);
-      this->reverse = reverse;
+      out = out * 85;
+      servoOut = (int)(90 - DEADZONE + out);
     }
+    //Serial.printf("servo out %f \n",(float)(servoOut -90)/ 180);
+    return servoOut;
+  }
 
-    void output(float speed){
-        if(this->reverse){
-          //speed = -speed;
-        }
-        this->targetSpeed = speed;
+  void update() {
+    //printf("pulses %d", this->pulses);
+    float instantaneousSpeed = this->pulses / (float)PULSES_PER_REV * 2.0 * PI * LINEAR_PER_REV * MOTOR_UPDATE_RATE;
+    if (this->reverse) {
+      instantaneousSpeed = -instantaneousSpeed;
     }
-    //stolen from igvc 21 / QEI library
-    void pulse(int left, int right)
-    {
-      //Serial.printf("left %d, right %d \n", left, right);
-      currState_ = (left << 1) | (right);
+    //Serial.printf("Instantaneuos speed %f \n", instantaneousSpeed);
+    this->speedEstimate += (1.0f - LPIIR_DECAY) * (instantaneousSpeed - this->speedEstimate);  // Low pass filter our speed estimate
+    float pidOUT = this->updatePID(this->targetSpeed, this->speedEstimate);
+    //Serial.printf("pid output %f \n", pidOUT);
+    currentOutput += pidOUT;
 
-      //11->00->11->00
-      //11->00->11->00 is counter clockwise rotation or "forward".
-            if ((prevState_ == 0x3 && currState_ == 0x0) ||
-                    (prevState_ == 0x0 && currState_ == 0x3)) {
-                this->pulses++;
-                this->delta++;
-            }
-            //10->01->10->01 is clockwise rotation or "backward".
-            else if ((prevState_ == 0x2 && currState_ == 0x1) ||
-                     (prevState_ == 0x1 && currState_ == 0x2)) {
-                this->pulses--;
-                this->delta--;
-            }
-            
-            prevState_ = currState_;
-            if (currentOutput < 0.01f && currentOutput > -0.01f) { // break
-              
-            }
+    currentOutput = clamp(currentOutput, -MAX_SPEED, MAX_SPEED);
+    //Serial.println("curr output");
+    //Serial.println(currentOutput);
+    this->pulses = 0;
+    int servoOut = out2servo(currentOutput);
+    motorServo.write(servoOut);
+  }
+  float getDistance() {
+    float temp = this->delta / PULSES_PER_REV * LINEAR_PER_REV * PI * 2.0 * 2.0;
+    this->delta = 0;
+    return temp;
+  }
+
+  float getSpeedEstimate() {
+    //Serial.printf("target speed: %f \n", this->targetSpeed);
+
+    return this->speedEstimate;
+  }
+
+  int getPulses() {
+    return this->pulses;
+  }
+
+  motor& operator=(float v) {
+    output(v);
+    return *this;
+  }
+  // Adapted (Stolen) from RobotLib :)
+  float updatePID(float target_state, float cur_state) {
+    // Declare local variables
+    float P, I, D;
+    float result;
+    float slope;
+    float dt;
+
+    // Get the time step
+    dt = 1.0f / MOTOR_UPDATE_RATE;
+
+    // Calculate error
+    this->error = target_state - cur_state;
+    //Serial.printf("error %f \n", this->error);
+
+    // Integrate error using trapezoidal Riemann sums
+    this->prevError = target_state - this->lastState;
+    this->integrator += 0.5f * (this->error + this->prevError) * dt;
+
+    // Find the slope of the error curve using secant approximation
+    slope = (cur_state - this->lastState) / dt;
+
+    // Apply PID gains
+    P = this->kp * this->error;
+    I = this->ki * this->integrator;
+    D = this->kd * slope;
+    I = clamp(I, -INCREMENT_AMT * .25, INCREMENT_AMT * .25);
+    // Sum P, I, D to get the result of the equation
+    // Bind the output if needed
+    result = clamp(P + I + D, -INCREMENT_AMT, INCREMENT_AMT);
+
+    // Update timing and increment to the next state
+    this->lastState = cur_state;
+
+    // Return the PID result
+    return result;
+  }
+
+  static float clamp(float val, float min, float max) {
+    if (val > max) {
+      return max;
+    } else if (val < min) {
+      return min;
     }
-    void brake(){
-      this->motorServo.write(90);
-    }
-    int out2servo(float out)
-    {
-      
-      if(this->reverse)
-        out = -out;
-      out = out / MAX_SPEED;
-      int servoOut;
-      //Serial.printf("servo in %f \n",out);
-      if (out < 0.01f && out > -0.01f)
-      { // break
-        servoOut = 90;
-      }
-      else if (currentOutput > 0.0f) 
-      { // forward
-        out = out * 85;
-        servoOut = (int)(90 + DEADZONE + out);
-      }
-      else //backward
-      {
-       out = out * 85;
-       servoOut = (int)(90 -DEADZONE + out);
-      }
-      //Serial.printf("servo out %f \n",(float)(servoOut -90)/ 180);
-      return servoOut;
-    }
-    
-    void update(){
-      //printf("pulses %d", this->pulses);
-      float instantaneousSpeed = this->pulses / (float)PULSES_PER_REV * 2.0 * PI * LINEAR_PER_REV * MOTOR_UPDATE_RATE;
-      if(this->reverse){
-        instantaneousSpeed = -instantaneousSpeed;
-      }
-      //Serial.printf("Instantaneuos speed %f \n", instantaneousSpeed);
-      this->speedEstimate += (1.0f - LPIIR_DECAY) * (instantaneousSpeed - this->speedEstimate); // Low pass filter our speed estimate
-      float pidOUT = this->updatePID(this->targetSpeed, this->speedEstimate);
-      //Serial.printf("pid output %f \n", pidOUT);
-      currentOutput += pidOUT;
+    return val;
+  }
 
-      currentOutput = clamp(currentOutput, -MAX_SPEED, MAX_SPEED);
-      //Serial.println("curr output");
-      //Serial.println(currentOutput);
-      this->pulses = 0;
-      int servoOut = out2servo(currentOutput);
-      motorServo.write(servoOut);
-      
-    }
-      float getDistance(){
-        float temp = this->delta / PULSES_PER_REV * LINEAR_PER_REV *  PI * 2.0 * 2.0;
-        this->delta = 0;
-        return temp;
-      }
-
-      float getSpeedEstimate() 
-      {
-       //Serial.printf("target speed: %f \n", this->targetSpeed);
-        
-        return this->speedEstimate;
-      }
-
-      int getPulses(){
-        return this->pulses;
-      }
-        
-      motor& operator= (float v) 
-      {
-        output(v);
-        return *this;
-      }
-            // Adapted (Stolen) from RobotLib :)
-        float updatePID(float target_state, float cur_state) {            
-            // Declare local variables
-            float P, I, D;
-            float result;
-            float slope;
-            float dt;
-            
-            // Get the time step
-            dt = 1.0f/MOTOR_UPDATE_RATE;
-            
-            // Calculate error
-            this->error = target_state - cur_state;
-            //Serial.printf("error %f \n", this->error);
-            
-            // Integrate error using trapezoidal Riemann sums
-            this->prevError = target_state - this->lastState;
-            this->integrator += 0.5f * (this->error + this->prevError) * dt;
-            
-            // Find the slope of the error curve using secant approximation
-            slope = (cur_state - this->lastState) / dt;
-            
-            // Apply PID gains
-            P = this->kp * this->error;
-            I = this->ki * this->integrator;
-            D = this->kd * slope;
-            I = clamp(I, -INCREMENT_AMT * .25, INCREMENT_AMT * .25);
-            // Sum P, I, D to get the result of the equation
-            // Bind the output if needed
-            result = clamp(P + I + D, -INCREMENT_AMT, INCREMENT_AMT);
-            
-            // Update timing and increment to the next state
-            this->lastState = cur_state;
-            
-            // Return the PID result
-            return result;
-        }
-        
-        static float clamp(float val, float min, float max)
-        {
-            if(val > max)
-            {
-                return max;
-            }
-            else if(val < min)
-            {
-                return min;
-            }
-            return val;
-        }
-
-        void resetError(){
-            this->error = 0.0f;
-            this->prevError = 0.0f;
-            this->lastState = 0.0f;
-        }
-      
-        
-    
-  
+  void resetError() {
+    this->error = 0.0f;
+    this->prevError = 0.0f;
+    this->lastState = 0.0f;
+  }
 };
 
 #endif
--- firmware/motor_control/motor_control.ino
+++ firmware/motor_control/motor_control.ino
@@ -1,7 +1,7 @@
 #include "TickTwo.h"
 #include <ACAN2515.h>
 #include "motor.h"
-#include "common.h" 
+#include "common.h"
 
 const int ledPin = LED_BUILTIN;
 const int CANLED = 0;
@@ -10,7 +10,7 @@ const int LED0 = 1;
 const int encoderLeftB = 3;
 const int encoderLeftA = 2;
 const int encoderRightA = 4;
-const int encoderRightB = 5; 
+const int encoderRightB = 5;
 
 const int leftMotorPwmPin = 12;
 const int rightMotorPwmPin = 14;
@@ -18,24 +18,27 @@ const int rightMotorPwmPin = 14;
 const int estopPin = 27;
 
 
-static const byte MCP2515_SCK  = 10 ; // SCK input of MCP2515
-static const byte MCP2515_MOSI = 11; // SDI input of MCP2515
-static const byte MCP2515_MISO = 8 ; // SDO output of MCP2515
+static const byte MCP2515_SCK = 10;   // SCK input of MCP2515
+static const byte MCP2515_MOSI = 11;  // SDI input of MCP2515
+static const byte MCP2515_MISO = 8;   // SDO output of MCP2515
 
-static const byte MCP2515_CS  = 9 ;  // CS input of MCP2515
-static const byte MCP2515_INT = 7 ;  // INT output of MCP2515
+static const byte MCP2515_CS = 9;   // CS input of MCP2515
+static const byte MCP2515_INT = 7;  // INT output of MCP2515
 
-ACAN2515 can (MCP2515_CS, SPI1, MCP2515_INT) ;
-static const uint32_t QUARTZ_FREQUENCY = 8UL * 1000UL * 1000UL ; // 8 MHz
+ACAN2515 can(MCP2515_CS, SPI1, MCP2515_INT);
+static const uint32_t QUARTZ_FREQUENCY = 8UL * 1000UL * 1000UL;  // 8 MHz
 
 CANMessage frame;
 
-// motor leftMotor(leftMotorPin, true);
-// motor rightMotor(rightMotorPin, false);
+
+motor leftMotor(leftMotorPwmPin, false);
+motor rightMotor(rightMotorPwmPin, false);
 
 void configureCan();
 void printCanMsg();
 void updateMsgData();
+void updateLeft();
+void updateRight();
 void setFiveMilliSecFlag();
 void setTenMilliSecFlag();
 void setFiftyMilliSecFlag();
@@ -48,60 +51,81 @@ short leftSpeedSetpoint;
 short rightSpeedSetpoint;
 
 
-TickTwo timer1(setFiveMilliSecFlag, 5); //TODO: CHANGE 5 ms PLACE HOLDERS
-TickTwo timer2(setTenMilliSecFlag, 10); //TODO: CHANGE 10 ms PLACE HOLDERS
-TickTwo timer3(setFiftyMilliSecFlag, 1000); //TODO: CHANGE 50 ms PLACE HOLDERS
+TickTwo timer1(setFiveMilliSecFlag, 5);      //TODO: CHANGE 5 ms PLACE HOLDERS
+TickTwo timer2(setTenMilliSecFlag, 10);      //TODO: CHANGE 10 ms PLACE HOLDERS
+TickTwo timer3(setFiftyMilliSecFlag, 1000);  //TODO: CHANGE 50 ms PLACE HOLDERS
 
-bool FIVE_MS_FLAG = false; //TODO : flag to keep track of 5 ms flag
-bool TEN_MS_FLAG = false; //TODO : flag to keep track of 10 ms flag
-bool FIFTY_MS_FLAG = false; //TODO: flag to keep track of 50 ms flag
+bool FIVE_MS_FLAG = false;   //TODO : flag to keep track of 5 ms flag
+bool TEN_MS_FLAG = false;    //TODO : flag to keep track of 10 ms flag
+bool FIFTY_MS_FLAG = false;  //TODO: flag to keep track of 50 ms flag
 
-static uint32_t gReceivedFrameCount = 0 ;
+static uint32_t gReceivedFrameCount = 0;
 
 
 void setup() {
-  pinMode(LED0, OUTPUT);
-  digitalWrite (LED0, HIGH);
-  pinMode(CANLED, OUTPUT);
-  digitalWrite (CANLED, HIGH);
-
   Serial.begin(9600);
   while (!Serial) {
-    delay (50) ;
-    digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
+  ; // wait for serial port to connect. Needed for native USB port only
   }
+
+  // NOTE: SPI 1 must be configured before the PWM is initilized (in this case CAN before left motor setup)
+  //pinMode(MCP2515_INT, INPUT);
+  //pinMode(leftMotorPwmPin, OUTPUT);
   configureCan();
+
+  leftMotor.setup();
+  rightMotor.setup();
+
+  
+  pinMode(LED0, OUTPUT);
+  pinMode(CANLED, OUTPUT);
+  pinMode(estopPin, INPUT_PULLDOWN);
+  
+
+  pinMode(encoderLeftA, INPUT);
+  pinMode(encoderLeftB, INPUT);
+  pinMode(encoderRightA, INPUT);
+  pinMode(encoderRightB, INPUT);
+
+
+  // attachInterrupt(encoderLeftA, updateLeft, CHANGE);
 
   timer1.start();
   timer2.start();
   timer3.start();
-  }
-
+}
 void loop() {
   updateTimers();
-  digitalWrite (LED0, LOW);
-  digitalWrite (CANLED, LOW);
+  digitalWrite(LED0, LOW);
+  digitalWrite(CANLED, LOW);
 
-  if(FIVE_MS_FLAG){ //TODO: CHANGE PLACE HOLDERS
+  // if(can.receive(frame)){
+  //   Serial.print ("  id: ");Serial.println (frame.id,HEX);
+  //   Serial.print ("  data: ");
+  //   for(int x=0;x<frame.len;x++) {
+  //     Serial.print (frame.data[x],HEX); Serial.print(":");
+  //   }
+  //   Serial.println ("");
+  // }
+
+
+
+  if (FIVE_MS_FLAG) {  //TODO: CHANGE PLACE HOLDERS
 
     // UpdateLeft();
     // UpdateRight();
 
     FIVE_MS_FLAG = false;
   }
-  if(TEN_MS_FLAG){
+  if (TEN_MS_FLAG) {
 
     // ReadCAN();
     // UpdatePID();
 
     FIVE_MS_FLAG = false;
   }
-  if(FIFTY_MS_FLAG){
-    if (can.receive (frame)) {
-      printCanMsg(frame);
-    }
+  if (FIFTY_MS_FLAG) {
     FIFTY_MS_FLAG = false;
-    
   }
 }
 
@@ -111,64 +135,74 @@ void updateTimers() {
   timer3.update();
 }
 
-void configureCan(){
-  SPI1.setSCK (MCP2515_SCK);
-  SPI1.setTX (MCP2515_MOSI);
-  SPI1.setRX (MCP2515_MISO);
-  SPI1.setCS (MCP2515_CS); 
+void configureCan() {
+  SPI1.setSCK(MCP2515_SCK);
+  SPI1.setTX(MCP2515_MOSI);
+  SPI1.setRX(MCP2515_MISO);
+  SPI1.setCS(MCP2515_CS);
   SPI1.begin();
 
 
-  Serial.println ("Configure ACAN2515") ;
-  ACAN2515Settings settings (QUARTZ_FREQUENCY, 100UL * 1000UL) ; // CAN bit rate 100 kb/s
-  const uint16_t errorCode = can.begin (settings,  canISR) ;
-  if(errorCode == 0){
-    Serial.print ("Actual bit rate: ") ;
-    Serial.print (settings.actualBitRate ()) ;
+  Serial.println("Configure ACAN2515");
+  ACAN2515Settings settings(QUARTZ_FREQUENCY, 100UL * 1000UL);  // CAN bit rate 100 kb/s
+  const uint16_t errorCode = can.begin(settings, canISR);
+  if (errorCode == 0) {
+    Serial.print("Actual bit rate: ");
+    Serial.print(settings.actualBitRate());
+  } else {
+    Serial.print(errorCode);
   }
+}
 
-
-} 
-
-void canISR(){
+void canISR() {
   can.isr();
-  can.receive(frame);
-  switch(frame.id){
+  can.receive(frame);  
+  switch (frame.id) {
     case 0:
-    Serial.print("ESTOP \n");
-    leftSpeedSetpoint = (short)frame.data[0]<<8 | (short)frame.data[1];
-    rightSpeedSetpoint = (short)frame.data[2]<<8 | (short)frame.data[3];
+      Serial.print("ESTOP \n");
+      leftSpeedSetpoint = (short)frame.data[0] << 8 | (short)frame.data[1];
+      rightSpeedSetpoint = (short)frame.data[2] << 8 | (short)frame.data[3];
 
-    break;
-    
+      break;
+
     case 10:
-    Serial.print("10 ID \n");
-    leftSpeedSetpoint = (frame.data[0]<<8 & 0xFF00) | frame.data[1];
-    rightSpeedSetpoint = (frame.data[2]<<8 & 0xFF00) | frame.data[3];
-    break;
-
+      Serial.print("10 ID \n");
+      leftSpeedSetpoint = (frame.data[0] << 8 & 0xFF00) | frame.data[1];
+      rightSpeedSetpoint = (frame.data[2] << 8 & 0xFF00) | frame.data[3];
+      break;
   }
   Serial.print("LEFT: ");
   Serial.println(leftSpeedSetpoint);
   Serial.print("RIGHT: ");
   Serial.println(rightSpeedSetpoint);
   //printCanMsg(frame);
-
 }
-void printCanMsg(CANMessage frame){
-    Serial.print ("  id: ");Serial.println (frame.id,HEX);
-    Serial.print ("  ext: ");Serial.println (frame.ext);
-    Serial.print ("  rtr: ");Serial.println (frame.rtr);
-    Serial.print ("  len: ");Serial.println (frame.len);
-    Serial.print ("  data: ");
-    for(int x=0;x<frame.len;x++) {
-      Serial.print (frame.data[x],HEX); Serial.print(":");
-    }
-    Serial.println ("");
+void printCanMsg(CANMessage frame) {
+  Serial.print("  id: ");
+  Serial.println(frame.id, HEX);
+  Serial.print("  ext: ");
+  Serial.println(frame.ext);
+  Serial.print("  rtr: ");
+  Serial.println(frame.rtr);
+  Serial.print("  len: ");
+  Serial.println(frame.len);
+  Serial.print("  data: ");
+  for (int x = 0; x < frame.len; x++) {
+    Serial.print(frame.data[x], HEX);
+    Serial.print(":");
+  }
+  Serial.println("");
 }
 
-void updateMsgData(){
+// void updateLeft(){
+//   //Serial.printf("left update \n");
+//   leftMotor.pulse(digitalRead(encoderLeftA),digitalRead(encoderLeftB));
+// }
+// void updateRight(){
+//   rightMotor.pulse(digitalRead(encoderRightA),digitalRead(encoderRightB));
+// }
 
+void updateMsgData() {
 }
 void setFiveMilliSecFlag() {
   FIVE_MS_FLAG = true;
