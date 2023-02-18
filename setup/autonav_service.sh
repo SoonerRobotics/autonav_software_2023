7109280bea3941f8d85678b9a9ad13880bdf63cb
Dylan Zemlin <dylan.zemlin@gmail.com>
2023-02-18 14:54:29 -0600

    Fix file name issues

--- setup/autonav_service.sh
+++ setup/autonav_service.sh
@@ -1,5 +1,5 @@
 export DISPLAY=:0
 source /opt/ros/humble/setup.bash
-source /home/autonav/autonav_software-2023/install/setup.bash
+source /home/autonav/autonav_software_2023/install/setup.bash
 
 ros2 launch autonav_launch managed_manual.xml
