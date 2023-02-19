c7bf2fdb92ad7928d87596a4afbadeaa1c99702e
Dylan Zemlin <dylan.zemlin@gmail.com>
2023-02-19 16:22:56 -0600

    Fixed various imu/serial issues

--- autonav_ws/src/autonav_serial/src/imu.py
+++ autonav_ws/src/autonav_serial/src/imu.py
@@ -16,8 +16,8 @@ def on_read_imu():
     if(not sensor.is_connected):
         sensor.connect("/dev/ttyUSB0", 115200)
 
-    acceleration = s.read_acceleration_measurements()
-    ypr = s.read_yaw_pitch_roll()
+    acceleration = sensor.read_acceleration_measurements()
+    ypr = sensor.read_yaw_pitch_roll()
 
     data = IMUData()
     data.accel_x = acceleration.accel_x
@@ -30,17 +30,22 @@ def on_read_imu():
 
     log = Log()
     log.file = "imu_data"
-    log.data = f"{dta.accel_x},{data.accel_y},{data.accel_z},{data.yaw},{data.pitch},{data.roll}"
+    log.data = f"{data.accel_x},{data.accel_y},{data.accel_z},{data.yaw},{data.pitch},{data.roll}"
     log_publisher.publish(log)
 
 def main():
-    global node, imu_publisher
+    global node, imu_publisher, log_publisher
 
     rclpy.init()
 
     node = rclpy.create_node("autonav_serial")
-    imu_publisher = node.create_publisher(MotorInput, "/autonav/odemetry/imu", 20)
+    imu_publisher = node.create_publisher(IMUData, "/autonav/odemetry/imu", 20)
     log_publisher = node.create_publisher(Log, "/autonav/logging", 20)
+
+    log = Log()
+    log.file = "imu_data"
+    log.data = "accel_x,accel_y,accel_z,yaw,pitch,roll"
+    log_publisher.publish(log)
 
     node.create_timer(0.5, on_read_imu())
 
--- autonav_ws/src/autonav_serial/src/serial.py
+++ autonav_ws/src/autonav_serial/src/serial.py
@@ -1,52 +1,18 @@
 #!/usr/bin/env python3
 
 import rclpy
-import threading
 import can
 import struct
-import serial
 
 from rclpy.node import Node
-from std_msgs.msg._string import String
+from autonav_msgs.msg import MotorInput
 
-from autonav_msgs.msg._motor_input import MotorInput
 
 node: Node = None
 canbus: can.Bus = None
 
-ESTOP_ID = 0
-MOBSTOP_ID = 1
-MOBSTART_ID = 9
 MOTOR_CONTROL = 10
-VELOCITY_FEEDBACK_ID = 11
-ODOMETRY_FEEDBACK_ID = 14
-
 MOTOR_OFFSET = 35
-
-class CANReadThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self, daemon = True)
-
-        self.odom_publisher = node.create_publisher("/autonav/deltaodom", String, 10)
-
-    def run(self):
-        while True:
-            msg = canbus.recv(timeout = 1)
-
-            if msg is None:
-                continue
-
-            if msg.arbitration_id == VELOCITY_FEEDBACK_ID:
-                left_speed, right_speed, max_speed = struct.unpack("bbB", msg.data)
-
-            if msg.arbitration_id == ODOMETRY_FEEDBACK_ID:
-                delta_theta, delta_y, delta_x = struct.unpack("hhh", msg.data)
-
-            if msg.arbitration_id ==  ESTOP_ID or msg.arbitration_id == MOBSTOP_ID:
-                print(f"Received Stop -> {msg.arbitration_id}")
-            
-            if msg.arbitration_id == MOBSTART_ID:
-                print("Received MobStart")
 
 
 def clamp(n, minn, maxn):
@@ -76,8 +42,6 @@ def main():
     node.create_subscription(MotorInput, "/autonav/MotorInput", onMotorInput, 10)
 
     canbus = can.ThreadSafeBus(bustype = "slcan", channel = "/dev/autonav-can-835", bitrate = 100000)    
-    # can_thread = CANReadThread()
-    # can_thread.start()
 
     rclpy.spin(node)
     canbus.shutdown()
