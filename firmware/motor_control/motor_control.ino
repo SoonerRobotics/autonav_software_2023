64fc09dd56e14de349be3e827680d58203011c63
j-exinia <exinia@ou.edu>
2023-02-20 17:41:57 -0600

    added working firmware

--- firmware/motor_control/motor_control.ino
+++ firmware/motor_control/motor_control.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #include "TickTwo.h"
 #include <ACAN2515.h>
 #include "motor.h"
@@ -7,10 +8,11 @@ const int ledPin = LED_BUILTIN;
 const int CANLED = 0;
 const int LED0 = 1;
 
-const int encoderLeftB = 3;
-const int encoderLeftA = 2;
-const int encoderRightA = 4;
-const int encoderRightB = 5;
+const int encoderLeftA = 3;
+const int encoderLeftB = 2;
+
+const int encoderRightA = 5;
+const int encoderRightB = 4;
 
 const int leftMotorPwmPin = 16;
 const int rightMotorPwmPin = 14;
@@ -29,9 +31,14 @@ ACAN2515 can(MCP2515_CS, SPI1, MCP2515_INT);
 static const uint32_t QUARTZ_FREQUENCY = 8UL * 1000UL * 1000UL;  // 8 MHz
 
 CANMessage frame;
+CANMessage outFrame;
+
+robotStatus_t roboStatus;
+distance motorDistances;
+motorCommand motoCommand;
 
 
-motor leftMotor(leftMotorPwmPin, false);
+motor leftMotor(leftMotorPwmPin, true);
 motor rightMotor(rightMotorPwmPin, false);
 
 void configureCan();
@@ -39,13 +46,18 @@ void printCanMsg();
 void updateMsgData();
 void updateLeft();
 void updateRight();
-void setFiveMilliSecFlag();
-void setTenMilliSecFlag();
-void setFiftyMilliSecFlag();
-void blink();
+void sendCanOdomMsgOut();
+void resetDelta(float& dxn, float& dyn, float& don);
 
 bool ledState;
 int counterUS;
+
+float dxn = 0;
+float dyn = 0;
+float don = 0;
+
+bool blinky = false;
+
 
 short leftSpeedSetpoint;
 short rightSpeedSetpoint;
@@ -53,42 +65,31 @@ short rightSpeedSetpoint;
 
 TickTwo timer1(setFiveMilliSecFlag, 5);      //TODO: CHANGE 5 ms PLACE HOLDERS
 TickTwo timer2(setTenMilliSecFlag, 10);      //TODO: CHANGE 10 ms PLACE HOLDERS
-TickTwo timer3(setFiftyMilliSecFlag, 1000);  //TODO: CHANGE 50 ms PLACE HOLDERS
-
-bool FIVE_MS_FLAG = false;   //TODO : flag to keep track of 5 ms flag
-bool TEN_MS_FLAG = false;    //TODO : flag to keep track of 10 ms flag
-bool FIFTY_MS_FLAG = false;  //TODO: flag to keep track of 50 ms flag
+TickTwo timer3(setFiftyMilliSecFlag, 50);  //TODO: CHANGE 50 ms PLACE HOLDERS CURRENTLY 1 SEC for debugging
 
 static uint32_t gReceivedFrameCount = 0;
 
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-  ; // wait for serial port to connect. Needed for native USB port only
-  }
+  delay(50);
 
   // NOTE: SPI 1 must be configured before the PWM is initilized (in this case CAN before left motor setup)
-  //pinMode(MCP2515_INT, INPUT);
-  //pinMode(leftMotorPwmPin, OUTPUT);
   configureCan();
 
   leftMotor.setup();
   rightMotor.setup();
 
-  
   pinMode(LED0, OUTPUT);
   pinMode(CANLED, OUTPUT);
   pinMode(estopPin, INPUT_PULLDOWN);
-  
-
-  pinMode(encoderLeftA, INPUT);
-  pinMode(encoderLeftB, INPUT);
   pinMode(encoderRightA, INPUT);
   pinMode(encoderRightB, INPUT);
+  pinMode(encoderLeftA, INPUT);
+  pinMode(encoderLeftB, INPUT);
 
-
-  // attachInterrupt(encoderLeftA, updateLeft, CHANGE);
+  attachInterrupt(encoderLeftA, updateLeft, CHANGE);
+  attachInterrupt(encoderRightA, updateRight, CHANGE);
 
   timer1.start();
   timer2.start();
@@ -96,35 +97,45 @@ void setup() {
 }
 void loop() {
   updateTimers();
-  digitalWrite(LED0, LOW);
-  digitalWrite(CANLED, LOW);
-
-  // if(can.receive(frame)){
-  //   Serial.print ("  id: ");Serial.println (frame.id,HEX);
-  //   Serial.print ("  data: ");
-  //   for(int x=0;x<frame.len;x++) {
-  //     Serial.print (frame.data[x],HEX); Serial.print(":");
-  //   }
-  //   Serial.println ("");
-  // }
-
-
-
+  
   if (FIVE_MS_FLAG) {  //TODO: CHANGE PLACE HOLDERS
-
-    // UpdateLeft();
-    // UpdateRight();
-
     FIVE_MS_FLAG = false;
   }
   if (TEN_MS_FLAG) {
+    leftMotor.update();
+    rightMotor.update();
 
-    // ReadCAN();
-    // UpdatePID();
+
+    // Serial.println("Right speed");
+    // Serial.println(rightMotor.getSpeedEstimate());
+    // Serial.println("Left speed");
+    // Serial.println(leftMotor.getSpeedEstimate());
 
     FIVE_MS_FLAG = false;
   }
-  if (FIFTY_MS_FLAG) {
+  if (FIFTY_MS_FLAG ) {
+    float left_distance = leftMotor.getDistance();
+    float right_distance = rightMotor.getDistance();
+    
+    dxn = dxn + (left_distance + right_distance) / 2 * cos(don);
+    dyn = dyn + (left_distance + right_distance) / 2 * sin(don); 
+    don = don + (right_distance - left_distance) * DIAMETER_FROM_CENTER_WHEEL / DISTANCE_BETWEEN_WHEELS; //diametrer of center of wheel and diameter between wheel TODO: no magic numbers
+
+    motorDistances.xn = (short)(dxn * SPEED_SCALE_FACTOR);
+    motorDistances.yn = (short)(dyn * SPEED_SCALE_FACTOR);
+    motorDistances.on = (short)(don * SPEED_SCALE_FACTOR);
+
+    //sendCanOdomMsgOut();
+
+    resetDelta(dxn, dyn, don);
+    
+    blinky = !blinky;
+    digitalWrite(LED0, blinky);
+
+    Serial.print("Right speed: ");
+    Serial.println(right_distance);
+    Serial.print("Left speed: ");
+    Serial.println(left_distance);
     FIFTY_MS_FLAG = false;
   }
 }
@@ -145,7 +156,8 @@ void configureCan() {
 
   Serial.println("Configure ACAN2515");
   ACAN2515Settings settings(QUARTZ_FREQUENCY, 100UL * 1000UL);  // CAN bit rate 100 kb/s
-  const uint16_t errorCode = can.begin(settings, canISR);
+  settings.mRequestedMode = ACAN2515Settings::NormalMode ; // Select Normal mode
+  const uint16_t errorCode = can.begin(settings, onCanRecieve );
   if (errorCode == 0) {
     Serial.print("Actual bit rate: ");
     Serial.print(settings.actualBitRate());
@@ -154,28 +166,61 @@ void configureCan() {
   }
 }
 
-void canISR() {
+void onCanRecieve() {
   can.isr();
   can.receive(frame);  
   switch (frame.id) {
     case 0:
       Serial.print("ESTOP \n");
-      leftSpeedSetpoint = (short)frame.data[0] << 8 | (short)frame.data[1];
-      rightSpeedSetpoint = (short)frame.data[2] << 8 | (short)frame.data[3];
+      roboStatus.eStop = 1;
+      roboStatus.mStop = 1;
+      roboStatus.mStart = 0;
+      leftSpeedSetpoint = 0;
+      rightSpeedSetpoint = 0;
+
+      break;
+    case 1:
+      roboStatus.eStop = 1;
+      roboStatus.mStop = 0;
+      roboStatus.mStart = 0;
+      leftSpeedSetpoint = 0;
+      rightSpeedSetpoint = 0;
 
       break;
 
     case 10:
       Serial.print("10 ID \n");
-      leftSpeedSetpoint = (frame.data[0] << 8 & 0xFF00) | frame.data[1];
-      rightSpeedSetpoint = (frame.data[2] << 8 & 0xFF00) | frame.data[3];
+      roboStatus.eStop = 0;
+      roboStatus.mStop = 0;
+      roboStatus.mStart = 1;
+      motoCommand = *(motorCommand*)(frame.data);     //Noah made me cry. I dont know what they did but I dont like it one bit - Jorge
+      leftMotor = motoCommand.setpointLeft/(float)SPEED_SCALE_FACTOR;
+      rightMotor = motoCommand.setpointRight/(float)SPEED_SCALE_FACTOR;
+      // leftSpeedSetpoint = (frame.data[0] << 8 & 0xFF00) | frame.data[1];
+      // rightSpeedSetpoint = (frame.data[2] << 8 & 0xFF00) | frame.data[3];
+      
       break;
   }
   Serial.print("LEFT: ");
   Serial.println(leftSpeedSetpoint);
   Serial.print("RIGHT: ");
   Serial.println(rightSpeedSetpoint);
-  //printCanMsg(frame);
+  printCanMsg(frame);
+}
+
+void sendCanOdomMsgOut(){
+  outFrame.id = ODOM_OUT_ID;
+  outFrame.len = ODOM_OUT_LEN;
+  memcpy(outFrame.data, &motorDistances, 6 );
+
+  const bool ok = can.tryToSend (frame) ;
+  if(ok){
+    Serial.print ("Sent: Odom Data");
+  }
+  else{
+    Serial.println ("Send failure") ;
+  }
+
 }
 void printCanMsg(CANMessage frame) {
   Serial.print("  id: ");
@@ -193,30 +238,22 @@ void printCanMsg(CANMessage frame) {
   }
   Serial.println("");
 }
-
-// void updateLeft(){
-//   //Serial.printf("left update \n");
-//   leftMotor.pulse(digitalRead(encoderLeftA),digitalRead(encoderLeftB));
-// }
-// void updateRight(){
-//   rightMotor.pulse(digitalRead(encoderRightA),digitalRead(encoderRightB));
-// }
-
+void updateLeft(){
+  //Serial.printf("Left update \n");
+  leftMotor.pulse(digitalRead(encoderLeftA),digitalRead(encoderLeftB));
+}
+void updateRight(){
+  //Serial.printf("Right update \n");
+  rightMotor.pulse(digitalRead(encoderRightA),digitalRead(encoderRightB));
+}
+void resetDelta(float &dxn, float &dyn, float &don){
+  dxn = 0;
+  dyn = 0;
+  don = 0;
+  motorDistances.xn = 0;
+  motorDistances.yn = 0;
+  motorDistances.on = 0;
+}
 void updateMsgData() {
 }
-void setFiveMilliSecFlag() {
-  FIVE_MS_FLAG = true;
-}
 
-void setTenMilliSecFlag() {
-  TEN_MS_FLAG = true;
-}
-
-void setFiftyMilliSecFlag() {
-  FIFTY_MS_FLAG = true;
-}
-
-void blink() {
-  digitalWrite(LED_BUILTIN, ledState);
-  ledState = !ledState;
-}
