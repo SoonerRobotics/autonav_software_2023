0991a354d819228e2e03a44ffaf8b903eda6d7e5
Dylan Zemlin <dylan.zemlin@gmail.com>
2023-02-21 13:00:50 -0600

    Start work on conbus system

---
+++ autonav_ws/src/autonav_libs/src/autonav.hpp
@@ -0,0 +1,15 @@
+namespace Autonav
+{
+    namespace ConBusAddress
+    {
+        const int MANUAL_CONTROL = 1;
+        const int IMU_PUBLISH_SPEED = 2;
+    }
+
+    namespace ConBusOpcode
+    {
+        const int READ = 0;
+        const int WRITE = 1;
+        const int READ_ALL = 2;
+    }
+}
