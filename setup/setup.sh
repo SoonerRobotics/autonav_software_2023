2cb9f25464a31c9bb1952e3653871dff5bbe451c
Dylan Zemlin <dylan.zemlin@gmail.com>
2023-02-23 15:08:49 -0600

    Submodule fixes

--- setup/setup.sh
+++ setup/setup.sh
@@ -12,16 +12,28 @@ bash vnav.sh
 # Steam Controller Dependencies
 bash steam.sh
 
-# Submodules
-git submodule update --remote --merge
-touch ../imgui/COLCON_IGNORE
-
 # ImGUI Dependencies
 sudo apt install libglfw3-dev libglew-dev -y
 
-# Rosdep
-# sudo apt-get install python3-rosdep2 -y
-# rosdep install --from-paths ../autonav_ws/src --ignore-src -y --rosdistro=humble
+# Python deps
+sudo apt install python3-pip -y
+pip3 install python-can[serial]
+
+# Copy the udev rules to the correct location
+sudo cp autonav.rules /etc/udev/rules.d/autonav.rules
+
+# Reload udev
+sudo service udev reload
+sleep 2
+sudo service udev restart
+
+# Copy services
+sudo cp autonav.service /etc/systemd/system/autonav.service
+sudo cp autonav_service.sh /usr/bin/autonav_service.sh
+
+# chmod time :D
+sudo chmod +x /usr/bin/autonav_service.sh
+sudo chmod 644 /etc/systemd/system/autonav.service
 
 # boot time
 echo "Use 'systemctl enable autonav' to enable the service at boot time."
