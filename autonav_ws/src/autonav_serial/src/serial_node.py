7a173bb55da21fd598b13d4e257c1b6de8bce653
Dylan Zemlin <dylan.zemlin@gmail.com>
2023-03-01 21:00:26 -0600

    Fix serial node not moving to ready

--- autonav_ws/src/autonav_serial/src/serial_node.py
+++ autonav_ws/src/autonav_serial/src/serial_node.py
@@ -7,7 +7,7 @@ from enum import Enum
 
 from rclpy.node import Node
 from autonav_msgs.msg import MotorInput
-from autonav_libs import AutoNode, Device, DeviceStateEnum as DeviceState
+from autonav_libs import AutoNode, Device, DeviceStateEnum as DeviceState, LogLevel
 
 
 def clamp(n, minn, maxn):
@@ -27,27 +27,30 @@ class SerialMotors(AutoNode):
 
     def setup(self):
         self.motor_subscriber = self.create_subscription(MotorInput, "/autonav/MotorInput", self.on_motor_input, 10)
-        self.canbus = None
         self.config.writeFloat(Registers.MOTOR_OFFSET.value, 35.0)
-        self.set_device_state(DeviceState.READY)
+        self.canbus = None
+        self.canbus_timer = self.create_timer(0.5, self.on_canbus_check)
+
+    def on_canbus_check(self):
+        if self.canbus is None:
+            try:
+                with open("/dev/autonav-can-835", "w") as f:
+                    pass
+
+                self.canbus = can.ThreadSafeBus(bustype="slcan", channel="/dev/autonav-can-835", bitrate=100000)
+            except:
+                if self.device_state != DeviceState.STANDBY:
+                    self.log("No CAN found, retrying in 0.5 second(s)", LogLevel.WARNING)
+                    self.set_device_state(DeviceState.STANDBY)
+                return
+
+        if self.canbus is not None and (self.device_state != DeviceState.READY and self.device_state != DeviceState.OPERATING):
+            self.set_device_state(DeviceState.READY)
+
 
     def on_motor_input(self, input: MotorInput):
         if self.device_state != DeviceState.OPERATING:
             return
-
-        if self.canbus is None:
-            self.canbus = can.ThreadSafeBus(bustype="slcan", channel="/dev/autonav-can-835", bitrate=100000)
-        
-            try:
-                with open("/dev/autonav-can-835", "r") as f:
-                    pass
-
-                if self.state != DeviceState.OPERATING:
-                    self.set_state(DeviceState.OPERATING)
-            except:
-                self.canbus = None
-                if self.state != DeviceState.STANDBY:
-                    self.set_state(DeviceState.STANDBY)
 
         left_speed = int(input.left_motor *
                          self.config.readFloat(Registers.MOTOR_OFFSET.value))
