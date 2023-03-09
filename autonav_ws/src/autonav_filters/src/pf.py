906a66d54de91d43301018ace2c526985ad205fe
Dylan Zemlin <dylan.zemlin@gmail.com>
2023-03-09 14:03:11 -0600

    More updates to particle filter

--- autonav_ws/src/autonav_filters/src/pf.py
+++ autonav_ws/src/autonav_filters/src/pf.py
@@ -12,7 +12,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class Particle:
-    def __init__(self, x=0, y=0, weight=0, theta=0):
+    def __init__(self, x=0, y=0, weight=1, theta=0):
         self.x = x
         self.y = y
         self.weight = weight
@@ -20,7 +20,8 @@ class Particle:
 
 
 class ParticleFilter:
-    def __init__(self):
+    def __init__(self, node):
+        self.m_node = node
         self.m_numParticles = 500
         self.m_gpsNoise = [0.4]
         self.m_odomNoise = [0.05, 0.05, 0.1]
@@ -29,24 +30,18 @@ class ParticleFilter:
         
         self.m_firstGps = None
         self.m_lastGps = None
-        self.m_lastPose = (0, 0, 0)
-        self.gps_points = []
+        self.m_gpsPoints = []
 
     def resetParticles(self):
-        self.m_particles = []
-        for i in range(self.m_numParticles):
-            particle = Particle(
-                0, 0, i / self.m_numParticles * 2 * math.pi, 0
-            )
-            self.m_particles.append(particle)
+        self.m_particles = [Particle(0, 0, theta = i / self.m_numParticles * 2 * math.pi) for i in range(self.m_numParticles)]
             
     def gpsToXY(self, gps: GPSFeedback):
         x = (gps.latitude - self.m_firstGps[0]) * 111086.2
         y = (self.m_firstGps[1] - gps.longitude) * 81978.2
         return (x, y)
     
-    def setFirstGPS(self, gps: GPSFeedback):
-        self.m_firstGps = (gps.latitude, gps.longitude)
+    def setFirstGPS(self, lat, lon):
+        self.m_firstGps = (lat, lon)
             
     def updateMotors(self, feedback: MotorFeedback):
         sum_x = 0
@@ -58,7 +53,8 @@ class ParticleFilter:
         for particle in self.m_particles:
             particle.x += feedback.delta_x * 1.2 * math.cos(particle.theta) + feedback.delta_y * math.sin(particle.theta)
             particle.y += feedback.delta_x * 1.2 * math.sin(particle.theta) + feedback.delta_y * math.cos(particle.theta)
-            particle.theta += feedback.delta_theta % (2 * math.pi)
+            particle.theta += feedback.delta_theta
+            particle.theta = particle.theta % (2 * math.pi)
             weight = math.pow(particle.weight, 2)
             
             sum_x += particle.x * weight
@@ -67,49 +63,49 @@ class ParticleFilter:
             sum_theta_y += math.sin(particle.theta) * weight
             sum_weight += weight
             
-        if sum_weight < 0.00001:
-            sum_weight = 0.00001
+        if sum_weight < 0.000001:
+            sum_weight = 0.000001
             
         avg_x = sum_x / sum_weight
         avg_y = sum_y / sum_weight
-        avg_theta = math.atan2(sum_theta_y, sum_theta_x) % (2 * math.pi)
+        avg_theta = math.atan2(sum_theta_y / sum_weight, sum_theta_x / sum_weight) % (2 * math.pi)
         return (avg_x, avg_y, avg_theta)
     
     def updateGPS(self, msg: GPSFeedback):
         if self.m_firstGps is None:
             self.m_firstGps = (msg.latitude, msg.longitude)
 
-        xy = self.gpsToXY(msg)
+        x, y = self.gpsToXY(msg)
         if self.m_lastGps is None:
-            self.m_lastGps = xy
+            self.m_lastGps = (x, y)
         
-        difx = xy[0] - self.m_lastGps[0]
-        dify = xy[1] - self.m_lastGps[1]
-        diftheta = math.atan2(dify, difx) % (2 * math.pi)
+        difx = x - self.m_lastGps[0]
+        dify = y - self.m_lastGps[1]
+        diftheta = math.atan2(difx, dify) % (2 * math.pi)
         
         for particle in self.m_particles:
-            distance = np.sqrt(math.pow(particle.x - xy[0], 2) + math.pow(particle.y - xy[1], 2))
+            distance = np.sqrt(math.pow(particle.x - x, 2) + math.pow(particle.y - y, 2))
             particle.weight = math.exp(-distance / (2 * math.pow(self.m_gpsNoise[0], 2)))
         
         self.shiftParticles()
-        self.m_lastGps = xy
-        self.gps_points.append(xy)
-        return xy
+        self.m_lastGps = (x, y)
+        self.m_gpsPoints.append((x, y))
+        return (x, y)
 
     def shiftParticles(self):
         weights = [particle.weight for particle in self.m_particles]
         total_weight = sum(weights)
-        if total_weight < 0.00001:
-            total_weight = 0.00001
+        if total_weight < 0.000001:
+            total_weight = 0.000001
         weights = [weight / total_weight for weight in weights]
         
         new_particles = random.choices(self.m_particles, weights, k=self.m_numParticles)
         self.m_particles = []
         
         for particle in new_particles:
-            randxy = (np.random.normal(0, self.m_odomNoise[0]), np.random.normal(0, self.m_odomNoise[1]))
-            x = particle.x + randxy[0] * math.cos(particle.theta) + randxy[1] * math.sin(particle.theta)
-            y = particle.y + randxy[0] * math.sin(particle.theta) + randxy[1] * math.cos(particle.theta)
+            randx, randy = (np.random.normal(0, self.m_odomNoise[0]), np.random.normal(0, self.m_odomNoise[1]))
+            x = particle.x + randx * math.cos(particle.theta) + randy * math.sin(particle.theta)
+            y = particle.y + randx * math.sin(particle.theta) + randy * math.cos(particle.theta)
             theta = np.random.normal(particle.theta, self.m_odomNoise[2]) % (2 * math.pi)
             self.m_particles.append(Particle(x, y, particle.weight, theta))
 
@@ -124,7 +120,7 @@ class ParticleFilterNode(AutoNode):
 
         self.m_firstGps = None
         self.m_collecting = True
-        self.m_pf = ParticleFilter()
+        self.m_pf = ParticleFilter(self)
 
     def setup(self):
         self.setDeviceState(DeviceStateEnum.READY)
@@ -134,9 +130,12 @@ class ParticleFilterNode(AutoNode):
             return
         
         if self.m_firstGps is None or self.m_collecting:
-            self.m_firstGps = (msg.latitude, msg.longitude)
-            self.log(f"Setting first GPS -> ({self.m_firstGps[0]}, {self.m_firstGps[1]})")
-            self.m_pf.setFirstGPS(msg)
+            if self.m_firstGps is None:
+                self.m_firstGps = (msg.latitude, msg.longitude)
+            else:
+                self.m_firstGps = (0.8 * self.m_firstGps[0] + 0.2 * msg.latitude, 0.8 * self.m_firstGps[1] + 0.2 * msg.longitude)
+            self.log(f"[Collecting] Setting GPS -> ({self.m_firstGps[0]}, {self.m_firstGps[1]})")
+            self.m_pf.setFirstGPS(self.m_firstGps[0], self.m_firstGps[1])
             return
         
         self.log(f"GPS -> ({msg.latitude}, {msg.longitude})")
@@ -153,11 +152,6 @@ class ParticleFilterNode(AutoNode):
         if self.m_firstGps is not None:
             point.x = self.m_firstGps[0] + avg_x / 111086.2
             point.y = self.m_firstGps[1] - avg_y / 81978.2
-            
-        if self.m_firstGps is not None:
-            lat = self.m_firstGps[0] + avg_x / 111086.2
-            lon = self.m_firstGps[1] - avg_y / 81978.2
-            # self.log(f"({lat}, {lon})")
         
         output.position = point
         self.m_posePublisher.publish(output)
