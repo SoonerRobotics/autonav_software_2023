1cd5a1f519cd500261ba11f4b25c7c4bf7440750
Dylan Zemlin <dylan.zemlin@gmail.com>
2023-03-14 19:53:05 -0500

    Add can read thread to serial

--- autonav_ws/src/autonav_serial/src/serial_node.py
+++ autonav_ws/src/autonav_serial/src/serial_node.py
@@ -6,7 +6,7 @@ import struct
 from enum import Enum
 
 from rclpy.node import Node
-from autonav_msgs.msg import MotorInput
+from autonav_msgs.msg import MotorInput, MotorFeedback
 from autonav_libs import AutoNode, Device, DeviceStateEnum as DeviceState, LogLevel
 
 
@@ -15,6 +15,7 @@ def clamp(n, minn, maxn):
 
 
 MOTOR_CONTROL_ID = 10
+MOTOR_FEEDBACK_ID = 14
 
 
 class Registers(Enum):
@@ -26,11 +27,40 @@ class SerialMotors(AutoNode):
         super().__init__(Device.SERIAL_CAN, "autonav_serial_can")
 
     def setup(self):
-        self.m_motorSubscriber = self.create_subscription(
-            MotorInput, "/autonav/MotorInput", self.on_motor_input, 10)
+        if self.m_canReadThread is not None:
+            self.m_canReadThread.join()
+
+        self.m_motorSubscriber = self.create_subscription(MotorInput, "/autonav/MotorInput", self.on_motor_input, 10)
+        self.m_feedbackPublisher = self.create_publisher(MotorFeedback, "/autonav/MotorFeedback", 10)
         self.config.writeFloat(Registers.MOTOR_OFFSET.value, 35.0)
         self.m_can = None
         self.m_canTimer = self.create_timer(0.5, self.canWorker)
+        self.m_canReadThread = self.create_thread(self.canThreadWorker)
+        self.m_canReadThread.setDaemon(True)
+    
+    def operate(self):
+        self.m_canReadThread.start()
+
+    def canThreadWorker(self):
+        while True:
+            if self.getDeviceState() != DeviceState.READY and self.getDeviceState() != DeviceState.OPERATING:
+                continue
+            if self.m_can is not None:
+                try:
+                    msg = self.m_can.recv(timeout = 1)
+                    if msg is not None:
+                        self.onCanMessageReceived(msg)
+                except can.CanError:
+                    self.log(LogLevel.ERROR, "CAN Error")
+
+    def onCanMessageReceived(self, msg):
+        if msg.arbitration_id == MOTOR_FEEDBACK_ID:
+            deltaTheta, deltaY, deltaX  = struct.unpack("hhh", msg.data)
+            feedback = MotorFeedback()
+            feedback.delta_theta = deltaTheta
+            feedback.delta_y = deltaY
+            feedback.delta_x = deltaX
+            self.m_feedbackPublisher.publish(feedback)  
 
     def canWorker(self):
         try:
@@ -57,14 +87,10 @@ class SerialMotors(AutoNode):
         if self.getDeviceState() != DeviceState.OPERATING:
             return
 
-        left_speed = int(input.left_motor *
-                         self.config.readFloat(Registers.MOTOR_OFFSET.value))
-        right_speed = int(input.right_motor *
-                          self.config.readFloat(Registers.MOTOR_OFFSET.value))
+        left_speed = int(input.left_motor * self.config.readFloat(Registers.MOTOR_OFFSET.value))
+        right_speed = int(input.right_motor * self.config.readFloat(Registers.MOTOR_OFFSET.value))
         packed_data = struct.pack("hh", left_speed, right_speed)
-
-        can_msg = can.Message(
-            arbitration_id=MOTOR_CONTROL_ID, data=packed_data)
+        can_msg = can.Message(arbitration_id=MOTOR_CONTROL_ID, data=packed_data)
 
         try:
             self.m_can.send(can_msg)
