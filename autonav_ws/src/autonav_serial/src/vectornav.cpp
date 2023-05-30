5b7a671ae2c19eafd6e763e5fef25d5112bdbce6
Dylan Zemlin <dylan.zemlin@gmail.com>
2023-05-30 16:48:01 -0500

    Convert vectornav to cpp

--- autonav_ws/src/autonav_serial/src/vectornav.cpp
+++ autonav_ws/src/autonav_serial/src/vectornav.cpp
@@ -1,5 +1,7 @@
 #include "scr_core/node.h"
 #include "rclcpp/rclcpp.hpp"
+#include "autonav_msgs/msg/gps_feedback.hpp"
+#include "autonav_msgs/msg/imu_data.hpp"
 
 #include "vn/sensors.h"
 #include "vn/thread.h"
@@ -18,6 +20,10 @@ public:
 
     void configure() override
     {
+        this->gpsFeedbackPublisher = this->create_publisher<autonav_msgs::msg::GPSFeedback>("/autonav/gps", 20);
+        this->imuDataPublisher = this->create_publisher<autonav_msgs::msg::IMUData>("/autonav/imu", 20);
+        auto timer = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&VectornavNode::onTimerTick, this));
+
         sensor.connect(VectornavConstants::SENSOR_PORT, VectornavConstants::SENSOR_BAUDRATE);
         auto modelNumber = sensor.readModelNumber();
         auto firmwareVersion = sensor.readFirmwareVersion();
@@ -33,36 +39,49 @@ public:
         this->setDeviceState(SCR::DeviceState::OPERATING);
     }
 
+    void onTimerTick()
+    {
+        if (this->getDeviceState() != SCR::DeviceState::OPERATING)
+        {
+            return;
+        }
+
+        auto gps = sensor.readGpsSolutionLla();
+        auto imu = sensor.readImuMeasurements();
+        auto ypr = sensor.readYawPitchRoll();
+
+        autonav_msgs::msg::GPSFeedback gpsFeedback;
+        gpsFeedback.latitude = gps.lla.x;
+        gpsFeedback.longitude = gps.lla.y;
+        gpsFeedback.altitude = gps.lla.z;
+        gpsFeedback.satellites = gps.numSats;
+        gpsFeedback.gps_fix = gps.gpsFix;
+        gpsFeedback.is_locked = gps.gpsFix == 0 ? false : true;
+
+        autonav_msgs::msg::IMUData imuData;
+        imuData.accel_x = imu.accel.x;
+        imuData.accel_y = imu.accel.y;
+        imuData.accel_z = imu.accel.z;
+        imuData.angular_x = imu.gyro.x;
+        imuData.angular_y = imu.gyro.y;
+        imuData.angular_z = imu.gyro.z;
+        imuData.yaw = ypr.x;
+        imuData.pitch = ypr.y;
+        imuData.roll = ypr.z;
+
+        this->gpsFeedbackPublisher->publish(gpsFeedback);
+        this->imuDataPublisher->publish(imuData);
+    }
+
 private:
     vn::sensors::VnSensor sensor;
+
+    rclcpp::Publisher<autonav_msgs::msg::GPSFeedback>::SharedPtr gpsFeedbackPublisher;
+    rclcpp::Publisher<autonav_msgs::msg::IMUData>::SharedPtr imuDataPublisher;
 };
 
 int main(int a, char** b)
 {
-    auto sensor = vn::sensors::VnSensor();
-    sensor.connect(VectornavConstants::SENSOR_PORT, VectornavConstants::SENSOR_BAUDRATE);
-    auto modelNumber = sensor.readModelNumber();
-    auto firmwareVersion = sensor.readFirmwareVersion();
-    auto serialNumber = sensor.readSerialNumber();
-    auto hardwareRevision = sensor.readHardwareRevision();
-
-    auto gps = sensor.readGpsSolutionLla();
-    auto imu = sensor.readImuMeasurements();
-
-    std::cout << "Connected to Vectornav IMU" << std::endl;
-    std::cout << "Model Number: " << modelNumber << std::endl;
-    std::cout << "Firmware Version: " << firmwareVersion << std::endl;
-    std::cout << "Serial Number: " << serialNumber << std::endl;
-    std::cout << "Hardware Revision: " << hardwareRevision << std::endl;
-
-    std::cout << "GPS: " << "(" << gps.lla.x << "," << gps.lla.y << "," << gps.lla.z << ")" << std::endl;
-
-    std::cout << "Acceleration: " << "(" << imu.accel.x << "," << imu.accel.y << "," << imu.accel.z << ")" << std::endl;
-    std::cout << "Angular Rate: " << "(" << imu.gyro.x << "," << imu.gyro.y << "," << imu.gyro.z << ")" << std::endl;
-    std::cout << "Magnetic Field: " << "(" << imu.mag.x << "," << imu.mag.y << "," << imu.mag.z << ")" << std::endl;
-    std::cout << "Temperature: " << imu.temp << std::endl;
-    std::cout << "Pressure: " << imu.pressure << std::endl;
-
     rclcpp::init(a, b);
     rclcpp::spin(std::make_shared<VectornavNode>());
     rclcpp::shutdown();
